39bb6cfdb8b26849554ae7eed9eb4fa29ab7a360
MuhammadAKJ <Muhammadakj11@gmail.com>
2024-07-25 05:56:01 +0100

    FIX[api apps urls and views]

--- app/api/urls.py
+++ app/api/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from .views import ProductListApiView, ProductDetailApiView
+from .views import *
 
 app_name = 'api'
 
 urlpatterns = [
     path('product-list/', ProductListApiView.as_view()),
     path('detail/<str:name>/<slug:slug>/', ProductDetailApiView.as_view()),
-
+    path('cart/add/<int:product_id>/', CartAddView.as_view()),
+    path('cart/remove/<int:product_id>/', CartRemoveView.as_view()),
+    path('cart/details/', CartDetailView.as_view()),
+    path('cart/clear/', CartClearView.as_view()),
 ]
